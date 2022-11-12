206660a8ffaa7350f441066d6150f83d74186412
sachin20glab <sachinwaghmode1000@gmail.com>
2022-11-12 21:41:49 +0530

    Three cards section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './component/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule, MatButtonModule
+    MatCardModule, MatButtonModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -7,6 +7,33 @@ export interface Card {
   button: string
 }
 
+export interface Card1 {
+  img: string,
+  title: string,
+  desc: string,
+  button: string
+}
+
+export interface Card2 {
+  img: string,
+  title: string,
+  desc: string,
+  button: string
+}
+
+export interface Card3 {
+  img: string,
+  title: string,
+  desc: string,
+  button: string
+}
+
+export interface Card4 {
+  img: string,
+  title: string,
+  desc: string,
+  button: string
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -52,6 +79,63 @@ export class HomeComponent implements OnInit {
       title: "Flights from Kolkata",
       desc: "Connect to your Virgin Atlantic flight to London and beyond from Kolkata.",
       button: "Flights from Kolkata"
+    }
+  ]
+
+  cards1: Card1[] = [
+    {
+      img: "https://content.virginatlantic.com/content/vaa/www/in/en/home.damAssetRender.0.html/content/dam/vaa/Site%20Imagery/homepage/idp/featured/new-york-promo.jpg",
+      title: "Flights to New York",
+      desc: "Explore New York's iconic buildings, baseball, world class shopping and eating and a great nightlife.",
+      button: "Show flight deals"
+    },
+    {
+      img: "https://content.virginatlantic.com/content/vaa/www/in/en/home.damAssetRender.0.html/content/dam/vaa/Site%20Imagery/destinations/usa/los-angeles/Hollywood%20sign.jpg",
+      title: "Flights to Los Angeles",
+      desc: "The City of Angels might be famous for its Hollywood stars, but there’s more going on here than celebrity.",
+      button: "Show flight deals"
+    },
+    {
+      img: "https://content.virginatlantic.com/content/vaa/www/in/en/home.damAssetRender.0.html/content/dam/vaa/Site%20Imagery/homepage/idp/featured/boston-promo.jpg",
+      title: "Flights to Boston",
+      desc: "Plan a trip to the birthplace of American democracy, with plenty of history and dramatic seasonal changes.",
+      button: "Show flight deals"
+    }
+  ]
+
+  cards2: Card2[] = [
+    {
+      img: "https://content.virginatlantic.com/content/vaa/www/in/en/home.damAssetRender.20210721T0606099400400.html/content/dam/vaa/email/refresh/abandonbrowse/carousel/premium.jpg",
+      title: "Fly Premium Economy",
+      desc: "Experience award-winning premium travel. It's the little touches here and there that help you truly enjoy your time in the air.",
+      button: "Find out more"
+    },
+    {
+      img: "https://content.virginatlantic.com/content/vaa/www/in/en/home.damAssetRender.20210125T1406107380500.html/content/dam/vaa/email/Upper.jpg",
+      title: "Discover Upper Class",
+      desc: "Experience the next level of luxurious travel, whether you're with us for business or pleasure, travelling in Virgin Atlantic Upper Class.",
+      button: "Find out more"
+    }
+  ]
+
+  cards3: Card3[] = [
+    {
+      img: "https://content.virginatlantic.com/content/vaa/www/in/en/home.damAssetRender.20181112T0815528070500.html/content/dam/vaa/Site%20Imagery/homepage/idp/featured/generic-promo-700x483.jpg",
+      title: "Our best student offer",
+      desc: "A great offer when you travel to study in the UK.",
+      button: "Find out more"
+    },
+    {
+      img: "https://content.virginatlantic.com/content/vaa/www/in/en/home.damAssetRender.20200807T1043203720400.html/content/dam/vaa/Site%20Imagery/homepage/idp/hero/desktop/Covid-19-homepage.jpg",
+      title: "You're in safe hands",
+      desc: "Take a look at everything we're doing to ensue you fly safe.",
+      button: "Find out more"
+    },
+    {
+      img: "https://content.virginatlantic.com/content/vaa/www/in/en/home.damAssetRender.20200807T1043203720400.html/content/dam/vaa/Site%20Imagery/homepage/idp/hero/desktop/Covid-19-homepage.jpg",
+      title: "COVID-19 Travel Advice",
+      desc: "Check the latest travel updates for your destination.",
+      button: "Find out more"
     }
   ]
 
